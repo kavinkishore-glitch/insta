f9bf8cc1cc0a1630b328993c1619bb933dfe5942
kavinkishore <k@gmail.com>
2025-04-15 11:59:00 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './assets/Components/Login/Login';
-import Signup from './assets/Components/Login/Signup/Signup';
 import Home from './assets/Components/Home/Home';
-
-
+import Signup from './assets/Components/Signup/Signup';
 
 
 function App() {
@@ -13,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/signup" />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/signup" element={<Signup/>} />
         <Route path="/home" element={<Home/>} />
         <Route path="/search" element={<div style={{ color: 'white', padding: '20px' }}>Search Page Coming Soon</div>} />
 
